99cb58b03bcdd647253329af74b457fd1780ae2d
Mike B Harris <mike.harris@xtreamlab.net>
2016-10-30 13:23:11 +0000

    Added examples to run script

--- run.sh
+++ run.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 echo "Building..."
-cobc --free AssertContains.cbl
-cobc --free AssertNotContains.cbl
-cobc --free AssertEquals.cbl
-cobc --free AssertNotEquals.cbl
+cobc -free AssertContains.cbl
+cobc -free AssertNotContains.cbl
+cobc -free AssertEquals.cbl
+cobc -free AssertNotEquals.cbl
 cobc -x --free COBOLTestSuiteTest.cbl
 echo "Running test suite..."
 ./COBOLTestSuiteTest
+echo "Running example tests..."
+echo "Customers using EVALUATE..."
+cd examples/Customer-using-Evaluate
+cobc -free Customers.cbl
+cobc -x -free -L ../.. CustomersTest.cbl
+./CustomersTest
+echo "Customers using ENTRY..."
+cd ../Customer-using-Entry
+cobc -free Customers.cbl
+cobc -x -free -L ../.. CustomersTest.cbl
+./CustomersTest
+cd ../..
 echo "Finished"
